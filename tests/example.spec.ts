bd8537d33b52c42e8848b34c533f9a2d1cd0ad97
michurin-konstantin <112470614+michurin-konstantin@users.noreply.github.com>
2022-09-04 03:00:53 +0300

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -95,3 +95,95 @@ test("basic test", async ({ page }, testInfo) => {
 });
 
 
+test.only('focus this test', async ({ page }) => {
+  // Run only focused tests in the entire project.
+});
+
+
+test.skip('skip this test', async ({ page }) => {
+  // This test is not run
+});
+
+
+test('skip this test', async ({ page, browserName }) => {
+  test.skip(browserName === 'firefox', 'Still working on it');
+});
+
+
+test.describe('two tests', () => {
+  test('one', async ({ page }) => {
+  });
+
+  test('two', async ({ page }) => {
+   
+  });
+});
+
+
+test('Test login page @fast', async ({ page }) => {
+  // ...
+});
+
+test('Test full report @slow', async ({ page }) => {
+  // ...
+});
+
+
+test.describe('chromium only', () => {
+  test.skip(({ browserName }) => browserName !== 'chromium', 'Chromium only!');
+
+  test.beforeAll(async () => {
+    // This hook is only run in Chromium.
+  });
+
+  test('test 1', async ({ page }) => {
+    // This test is only run in Chromium.
+  });
+
+  test('test 2', async ({ page }) => {
+    // This test is only run in Chromium.
+  });
+});
+
+
+const REPO = 'playwrite';
+const USER = 'michurin-konstantin';
+
+// Request context is reused by all tests in the file.
+let apiContext;
+
+test.beforeAll(async ({ playwright }) => {
+  apiContext = await playwright.request.newContext({
+    // All requests we send go to this API endpoint.
+    baseURL: 'https://api.github.com',
+    extraHTTPHeaders: {
+      // We set this header per GitHub guidelines.
+      'Accept': 'application/vnd.github.v3+json',
+      // Add authorization token to all requests.
+      // Assuming personal access token available in the environment.
+      'Authorization': `token ${process.env.API_TOKEN}`,
+    },
+  });
+})
+
+test.afterAll(async ({ }) => {
+  // Dispose all responses.
+  await apiContext.dispose();
+});
+
+test('last created issue should be first in the list', async ({ page }) => {
+  const newIssue = await apiContext.post(`/repos/${USER}/${REPO}/issues`, {
+    data: {
+      title: '[Feature] request 1',
+    }
+  });
+  expect(newIssue.ok()).toBeTruthy();
+
+  await page.goto(`https://github.com/${USER}/${REPO}/issues`);
+  const firstIssue = page.locator(`a[data-hovercard-type='issue']`).first();
+  await expect(firstIssue).toHaveText('[Feature] request 1');
+});
+
+
+
+
