db74bc71e731242e5cb6c77ebd6f16a3736eb7c1
michurin-konstantin <112470614+michurin-konstantin@users.noreply.github.com>
2022-09-04 03:16:26 +0300

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -144,43 +144,43 @@ test("basic tes88t", async ({ page }, testInfo) => {
 // });
 
 
-const REPO = 'playwrite';
-const USER = 'michurin-konstantin';
+// const REPO = 'playwrite';
+// const USER = 'michurin-konstantin';
 
-// Request context is reused by all tests in the file.
-let apiContext;
+// // Request context is reused by all tests in the file.
+// let apiContext;
 
-test.beforeAll(async ({ playwright }) => {
-  apiContext = await playwright.request.newContext({
-    // All requests we send go to this API endpoint.
-    baseURL: 'https://api.github.com',
-    extraHTTPHeaders: {
-      // We set this header per GitHub guidelines.
-      'Accept': 'application/vnd.github.v3+json',
-      // Add authorization token to all requests.
-      // Assuming personal access token available in the environment.
-      'Authorization': `token ${process.env.API_TOKEN}`,
-    },
-  });
-})
+// test.beforeAll(async ({ playwright }) => {
+//   apiContext = await playwright.request.newContext({
+//     // All requests we send go to this API endpoint.
+//     baseURL: 'https://api.github.com',
+//     extraHTTPHeaders: {
+//       // We set this header per GitHub guidelines.
+//       'Accept': 'application/vnd.github.v3+json',
+//       // Add authorization token to all requests.
+//       // Assuming personal access token available in the environment.
+//       'Authorization': `token ${process.env.API_TOKEN}`,
+//     },
+//   });
+// })
 
-test.afterAll(async ({ }) => {
-  // Dispose all responses.
-  await apiContext.dispose();
-});
+// test.afterAll(async ({ }) => {
+//   // Dispose all responses.
+//   await apiContext.dispose();
+// });
 
-test('last created issue should be first in the list', async ({ page }) => {
-  const newIssue = await apiContext.post(`/repos/${USER}/${REPO}/issues`, {
-    data: {
-      title: '[Feature] request 1',
-    }
-  });
-  expect(newIssue.ok()).toBeTruthy();
+// test('last created issue should be first in the list', async ({ page }) => {
+//   const newIssue = await apiContext.post(`/repos/${USER}/${REPO}/issues`, {
+//     data: {
+//       title: '[Feature] request 1',
+//     }
+//   });
+//   expect(newIssue.ok()).toBeTruthy();
 
-  await page.goto(`https://github.com/${USER}/${REPO}/issues`);
-  const firstIssue = page.locator(`a[data-hovercard-type='issue']`).first();
-  await expect(firstIssue).toHaveText('[Feature] request 1');
-});
+//   await page.goto(`https://github.com/${USER}/${REPO}/issues`);
+//   const firstIssue = page.locator(`a[data-hovercard-type='issue']`).first();
+//   await expect(firstIssue).toHaveText('[Feature] request 1');
+// });
 
 
 
